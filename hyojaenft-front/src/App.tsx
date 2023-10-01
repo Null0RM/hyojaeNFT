9652c908b8a57d9390ceac0f86545ce6637b85d3
jayming66 <mjw8941@gmail.com>
2023-10-01 15:19:40 +0900

    환경 세팅

---
+++ hyojaenft-front/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body>
+        <h1>jaewi Hi</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
