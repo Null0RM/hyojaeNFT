0aafc029f80d6b338974ebc01d8e36b6fae86c32
jayming66 <mjw8941@gmail.com>
2023-10-01 15:26:22 +0900

    feat : Home page 추가

--- hyojaenft-front/src/App.tsx
+++ hyojaenft-front/src/App.tsx
@@ -1,8 +1,11 @@
+import { Home } from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <body>
+        <Home></Home>
         <h1>jaewi Hi</h1>
       </body>
     </div>
